53e99bfcfbed483eccf5169164ce1af86f8b03f3
Danil <157003107+sp1k13@users.noreply.github.com>
2024-03-25 19:32:35 +0200

    Update addCompetition.php

--- admin/addCompetition.php
+++ admin/addCompetition.php
@@ -237,7 +237,6 @@ if (isset($_POST["submit"])) {
         '    echo "0 results";' . "\n" .
         '}' . "\n" .
         '?>' . "\n" .
-        '<p><span style="font-size: 20px;">Додаткову інформацю ви зможете знайти на <a href="https://italent.org.ua/nomination/2d-graphics">цьому сайті</a>.</span></p>' .
         '    </div>' .
         '    <div class="pop_up" id="pop_up">' .
         '    <div class="pop_up_container">' .
